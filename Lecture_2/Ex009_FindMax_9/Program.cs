51df0f3ef4bce6cfb08fb948f72316a318efbecd
Nikita <nikvs@mail.ru>
2022-07-10 19:08:58 +0700

    add Lecture_2. function & array

---
+++ Lecture_2/Ex009_FindMax_9/Program.cs
@@ -0,0 +1,101 @@
+﻿// Поиск максимального значения из 9 при помощи функций
+
+Console.Clear();
+
+// решение с помощью функции
+
+// проверка на наличие символов в строке s, отличных от цифр. возвращает false, если строка пустая или
+// содержит символы - не цифры
+bool DigitsOnly(string s)
+{
+    // проверка на пустую строку    
+    if (s.Length == 0) return false;
+    // foreach - цикл для каждого из
+    foreach (char c in s)
+    {
+        if (c < '0' || c > '9')
+            return false;
+    }
+    return true;
+}
+
+// ввод значения. запрашивает ввод, конвертирует в int32 и возвращает это число
+int GetData()
+{
+    bool testDigits = false;
+    string inputString = "";
+    while (!testDigits)
+    {
+        Console.Write("Введите значение: ");
+        inputString = Console.ReadLine();
+        testDigits = DigitsOnly(inputString);
+    }
+    return Convert.ToInt32(inputString);
+}
+
+// поиск максимального значения из трёх int
+int Max(int num1, int num2, int num3)
+{
+    int max = 0;
+    if (num1 > max) max = num1;
+    if (num2 > max) max = num2;
+    if (num3 > max) max = num3;
+    return max;
+}
+
+int result = Max(
+                Max(GetData(), GetData(), GetData()),
+                Max(GetData(), GetData(), GetData()),
+                Max(GetData(), GetData(), GetData())
+                );
+Console.WriteLine("Максимальное число: " + result);
+
+
+
+// лобовое решение (лапша)
+// Console.Write("Введите значение: ");
+// // Convert.ToInt32(string) - преобразование строки в Int32 (float, double, Int16 и т.д.)
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение: ");
+// int d = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение: ");
+// int e = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение: ");
+// int f = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение: ");
+// int g = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение: ");
+// int h = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение: ");
+// int i = Convert.ToInt32(Console.ReadLine());
+
+// int max = 0;
+
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// if (d > max) max = d;
+// if (e > max) max = e;
+// if (f > max) max = f;
+// if (g > max) max = g;
+// if (h > max) max = h;
+// if (i > max) max = i;
+
+// Console.WriteLine("Максимальное значение: " + max);
+
+
+
+
+
---
+++ Lecture_2/Ex010_Array/Program.cs
@@ -0,0 +1,101 @@
+﻿// работа с массивами при помощи функций
+Console.Clear();
+
+// проверка на наличие символов в строке s, отличных от цифр. возвращает false, если строка пустая или
+// содержит символы - не цифры
+bool DigitsOnly(string s)
+{
+    // проверка на пустую строку    
+    if (s.Length == 0) return false;
+    // foreach - цикл для каждого из
+    foreach (char c in s)
+    {
+        if (c < '0' || c > '9')
+            return false;
+    }
+    return true;
+}
+
+// ввод значения. запрашивает ввод, конвертирует в int32 и возвращает это число
+int GetData()
+{
+    bool testDigits = false;
+    string inputString = "";
+    while (!testDigits)
+    {
+        Console.Write("Введите значение: ");
+        inputString = Console.ReadLine();
+        testDigits = DigitsOnly(inputString);
+    }
+    return Convert.ToInt32(inputString);
+}
+
+// поиск максимального значения из массива
+int Max(int[] array)
+{
+    int n = array.Length;
+    int max = 0;
+    int index = 0;
+    while (index < n)
+    {
+        if (array[index] > max) max = array[index];
+        index++;
+    }
+    return max;
+}
+
+// поиск номера элемента массива по его значению. если элемента в массиве нет, то возвращает -1
+int IndexOf(int[] array, int find)
+{
+    int n = array.Length;
+    int position = -1;
+    int index = 0;
+    while (index < n)
+    {
+        if (array[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+// заполнение массива случайными числами от 1 до 9. void - ничего не возвращающий метод. в этом случае return не нужен
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+// печать массива
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+
+// новый int массив из 10 элементов
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+int result = Max(array);
+Console.WriteLine("Максимальное число: " + result);
+
+int pos = IndexOf(array, result);
+Console.WriteLine("Номер элемента в массиве: " + pos);
