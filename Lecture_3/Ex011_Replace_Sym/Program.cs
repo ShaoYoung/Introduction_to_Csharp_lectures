b1adf336a245a60283c2035d1f6aa47460c495c2
Nikita <nikvs@mail.ru>
2022-07-18 02:54:40 +0700

    add lecture_3

---
+++ Lecture_3/Ex011_Replace_Sym/Program.cs
@@ -0,0 +1,47 @@
+﻿//--Работа с текстом
+//Дан текст. В тексте нужно все пробелы заменить черточками,
+//маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+//маленькими “с”.
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+                + "ежели бы вас послали вместо нашего милого "
+                + "Винценгероде, вы бы взяли приступом согласие "
+                + "прусского короля. Вы так красноречивы. Вы "
+                + "дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+//метод принимает текст и меняет в нём символы oldValue на newValue
+string Replace(string text, char oldValue, char newValue)
+{
+    //String.Empty - пустая строка
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue)
+            result = result + $"{newValue}";
+        else
+            result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+Console.Clear();
+//аргументы метода можно указывать не по порядку, а по их имени
+//'' - char, "" - string
+string newText = Replace(oldValue: ' ', newValue: '|', text: text);
+Console.WriteLine(newText);
+Console.WriteLine();
+
+//а можно и по порядку и без имени
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+Console.WriteLine();
+
---
+++ Lecture_3/Ex012_Sort_Array/Program.cs
@@ -0,0 +1,115 @@
+﻿//Сортировка массива
+
+int[] arr = { 1, 3, 5, 8, 9, 2, 73, 3, 44, 55 };
+
+//метод печатает массив в строку
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+//метод сортирует от min до max массив, передаваемый в качестве аргумента. возвращает сортированный массив
+int[] SortMinMax(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPozition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPozition])
+                minPozition = j;
+        }
+        //swap (обмен)
+        int temp = array[i];
+        array[i] = array[minPozition];
+        array[minPozition] = temp;
+    }
+    return (array);
+}
+
+//метод сортирует от max до min массив, передаваемый в качестве аргумента. возвращает сортированный массив
+int[] SortMaxMin(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPozition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPozition])
+                maxPozition = j;
+        }
+        //swap (обмен)
+        int temp = array[i];
+        array[i] = array[maxPozition];
+        array[maxPozition] = temp;
+    }
+    return (array);
+}
+
+//метод ищет максимум в массиве. возвращает значение максимального элемента
+int FindMax(int[] array)
+{
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+        //swap (обмен)
+    }
+    return (max);
+}
+
+//метод ищет минимум в массиве. возвращает значение минимального элемента
+int FindMin(int[] array)
+{
+    int min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+            min = array[i];
+        //swap (обмен)
+    }
+    return (min);
+}
+
+//метод считает сумму цифр вводимого целого положительного числа
+int SumNumbers(int number)
+{
+    int sumNum = 0;
+    //int в string
+    string numberString = Convert.ToString(number);
+    //string в массив char
+    char[] numberChar = numberString.ToCharArray();
+    for (int i = 0; i < numberChar.Length; i++)
+    {
+        //char -> String -> int
+        sumNum = sumNum + Convert.ToInt32(Convert.ToString(numberChar[i]));
+    }
+    return sumNum;
+}
+
+Console.Clear();
+PrintArray(arr);
+
+int[] minMaxArray = SortMinMax(arr);
+PrintArray(minMaxArray);
+
+int[] maxMinArray = SortMaxMin(arr);
+PrintArray(maxMinArray);
+
+int maxInArray = FindMax(arr);
+Console.WriteLine("Максимальное значение в массиве: " + maxInArray);
+
+int minInArray = FindMin(arr);
+Console.WriteLine("Минимальное значение в массиве: " + minInArray);
+
+Console.Write("Введите положительное целое число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр числа: {number} равна {SumNumbers(number)}");
+
+
